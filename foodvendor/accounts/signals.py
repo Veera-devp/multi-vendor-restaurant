18bb0ed80d5526e0d98c6c26d390be7fd061dcf3
Veera-devp <saikam.raghava@gmail.com>
2023-11-29 16:41:58 +0530

    All fields entry and changed CSS

--- foodvendor/accounts/signals.py
+++ foodvendor/accounts/signals.py
@@ -1,23 +1,19 @@
-from django.db.models.signals import post_save,pre_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import User,UserProfile
 
-@receiver(post_save, sender=User)
-def post_save_create_profile_receiver(sender, instance, created, **kwargs):
+@receiver(post_save,sender=User)
+def post_save_create_profile_receiver(sender,instance,created,**kwargs):
     print(created)
     if created:
         UserProfile.objects.create(user=instance)
-        print('create the User Profile')
+        # print("user profile is created")
+     
     else:
         try:
             profile = UserProfile.objects.get(user=instance)
             profile.save()
-        except UserProfile.DoesNotExist:
+        except:
+            #Create the user profile if not existed
             UserProfile.objects.create(user=instance)
-            print('profile did not exist, but I created one')
-        print('user is updated')
-
-
-@receiver(pre_save,sender=User)
-def pre_save_profile_receiver(sender,instance,**kwargs):
-     pass
+            # print("profile didn't exist , But i created One")
--- foodvendor/accounts/views.py
+++ foodvendor/accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .forms import UserForm
 from .models import User
+from django.contrib import messages
 # Create your views here.
 
 def registerUser(request):
@@ -9,12 +10,25 @@ def registerUser(request):
             print(request.POST)
             form = UserForm(request.POST)
             if form.is_valid():
+                  # password = form.cleaned_data['password']
+                  # user = form.save(commit=False)
+                  # user.set_password(password)
+                  # user.role = User.CUSTOMER
+                  # user.save()
+
+                  first_name = form.cleaned_data['first_name']
+                  last_name = form.cleaned_data['last_name']
+                  username = form.cleaned_data['username']
+                  email = form.cleaned_data['email']
                   password = form.cleaned_data['password']
-                  user = form.save(commit=False)
-                  user.set_password(password)
-                  user.role = User.CUSTOMER
+                  user = User.objects.create_user(first_name=first_name,last_name=last_name,email=email,username=username,password=password)
+                  user.role=User.CUSTOMER
                   user.save()
+                  messages.success(request,'Your account has been registered succefully')
                   return redirect('registerUser')
+            else:
+                  print('Invalid Form')
+                  print(form.errors)
       else:
             form = UserForm()
       form = UserForm()
