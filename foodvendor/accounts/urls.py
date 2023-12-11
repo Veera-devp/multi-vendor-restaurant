c3323a320eab8f2888a703cd66103a2eab63a539
Veera-devp <saikam.raghava@gmail.com>
2023-12-11 20:44:58 +0530

    Setting up profile

--- foodvendor/accounts/urls.py
+++ foodvendor/accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
 urlpatterns = [
       path('registerUser/',views.registerUser, name='registerUser'),
@@ -15,5 +15,7 @@ urlpatterns = [
       path('forgot_password/',views.forgot_password,name='forgot_password'),
       path('reset_password_validate/<str:uidb64>/<str:token>',views.reset_password_validate,name='reset_password_validate'),
       path('reset_password/',views.reset_password,name='reset_password'),
+
+      path('vendor/', include('vendor.urls')),
 
 ]
