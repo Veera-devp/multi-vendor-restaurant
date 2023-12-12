bea7882a64c5c5a63807b2a63b84ca7acac02eca
Veera-devp <saikam.raghava@gmail.com>
2023-12-12 16:32:17 +0530

    vendor dahsboard setup

--- foodvendor/accounts/views.py
+++ foodvendor/accounts/views.py
@@ -10,6 +10,7 @@ from django.core.exceptions import PermissionDenied
 from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import message
+from vendor.models import Vendor
 # Create your views here.
 
 def check_role_vendor(user):
@@ -169,7 +170,11 @@ def custDashboard(request):
 @login_required(login_url='login')
 @user_passes_test(check_role_vendor)
 def vendorDashboard(request):
-    return render(request,'accounts/vendorDashboard.html')
+    vendor = Vendor.objects.get(user=request.user)
+    context = {
+        'vendor':vendor,
+    }
+    return render(request,'accounts/vendorDashboard.html',context)
 
 def forgot_password(request):
     if request.method == 'POST':
